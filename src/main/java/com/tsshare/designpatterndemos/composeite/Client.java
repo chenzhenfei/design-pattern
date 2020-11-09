ec4248b7a86abbaca5c4f4141b15cc2521d44939
chenzhenfei <11100247>
2020-11-09 19:08:58 +0800

    1、享元模式 - 数据库连接池

---
+++ src/main/java/com/tsshare/designpatterndemos/composeite/Client.java
@@ -0,0 +1,18 @@
+package com.tsshare.designpatterndemos.composeite;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author : zfchen
+ * @version 1.0
+ * @Description: TODO
+ * @date : 2020-11-09 14:54
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        Map map =new HashMap();
+        map.putAll(new HashMap());
+    }
+}
